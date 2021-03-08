cb8827122f25e7a8ab3b7916190d635496373f15
mohammadAqel <mohammedaqel100@gmail.com>
2021-03-08 16:32:22 +0200

    hello

---
+++ lab06b.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function alertFunction() {
+    alert("WELCOME");
+}
+
+alertFunction();
+
+function math() {
+
+    var x = 3.32;
+    var y = 2.23;
+    math = (x * y - y / x);
+}
+math();
+
+document.write(math);
+
+prompt("please enter the number");
+
+var color = prompt("enter a color")
+if (color == red) {
+    alert("click-right");
+} else {
+    alert("click-lift");
+}
+document.write(color);
